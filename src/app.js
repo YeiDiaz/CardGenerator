37807fc0b9d0adb9efaad704587ee051bb734e74
YeiDiaz <yei.diaz93@gmail.com>
2021-04-10 23:10:35 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let theLayer = ["&loz;", "&spades;", "&clubs;", "&diams;", "&hearts;"];
+  let theNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomSuits() {
+    return theLayer[Math.floor(Math.random() * theLayer.length)];
+  }
+
+  var theSuits = randomSuits();
+
+  function randomNumber() {
+    return theNumber[Math.floor(Math.random() * theNumber.length)];
+  }
+
+  if (theSuits == "&diams;" || theSuits == "&hearts;") {
+    document.querySelector(".top-layer").style.color = "red";
+    document.querySelector(".numbers").style.color = "red";
+    document.querySelector(".bottom-layer").style.color = "red";
+  }
+
+  document.querySelector(".top-layer").innerHTML = theSuits;
+
+  document.querySelector(".numbers").innerHTML = randomNumber();
+
+  document.querySelector(".bottom-layer").innerHTML = theSuits;
 };
